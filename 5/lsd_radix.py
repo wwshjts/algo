87bd85d4890e33c17c60daa5ad90f271420814e6
y.kareba <y.kareba@g.nsu.ru>
2023-04-15 16:07:23 +0700

    made some tests

--- 5/lsd_radix.py
+++ 5/lsd_radix.py
@@ -1,24 +1,33 @@
+from random import randint
+a_code = 97
+alphas = 25
+def test_sort(algh):
+    a = ['wwww', 'oooo', 'wwww']
+    print(f'testing {algh.__name__}')
+    print(f'test case 1: {"correct" if sorted(a) == algh(a) else "false"}')
+    for i in range(1, 10 + 1):
+        a = [''.join([chr(x + 97) for x in range(1, alphas)]) for _ in range(10)]
+        print(f'test case {i + 1}: {"correct" if sorted(a) == algh(a) else "false"}')
 def counting_sort(a, digit):
-    tmp = [0]*28
+    tmp = [0]*alphas
     res = [0] * len(a)
     for word in a:
-        tmp[ord(word[digit]) - 97] += 1
+        tmp[ord(word[digit]) - a_code] += 1
     #теперь каждый элемент tmp будет хранить количество эл-тов
     #меньше или равных себе
-    for i in range(1, 28):
+    for i in range(1, alphas):
         tmp[i] = tmp[i] + tmp[i - 1]
     #тем что мы идем с конца массива обеспечивается стабильность сортировки
     for i in range(len(a) - 1, -1, -1):
-        res[tmp[ord(a[i][digit]) - 97] - 1] = a[i]
-        tmp[ord(a[i][digit]) - 97] -= 1
+        res[tmp[ord(a[i][digit]) - a_code] - 1] = a[i]
+        tmp[ord(a[i][digit]) - a_code] -= 1
     return res
 
 def lsd_radix_sort(a):
+    '''lsd_radix_sort'''
     res = a.copy()
     dg = len(a[0])
     for i in range(dg - 1, -1, -1):
         res = counting_sort(res, i)
     return res
-a = ['afbd', 'sdfa', 'aaaa', 'bbdk']
-b = lsd_radix_sort(a)
-print(b)
+test_sort(lsd_radix_sort)
