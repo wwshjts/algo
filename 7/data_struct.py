d14b94c3801a6bd11a9c29a0c39d57458cea24a3
y.kareba <y.kareba@g.nsu.ru>
2023-04-16 15:59:43 +0700

    Done minimal heap

--- 7/data_struct.py
+++ 7/data_struct.py
@@ -107,7 +107,7 @@ class Vector:
         self.__vector[i] = val
 
 
-class BinaryHeap:
+class BinaryHeapMax:
     def __init__(self, size = 1):
         self.a = Vector(size = size)
     #return's list of existing leaves indexes
@@ -157,24 +157,79 @@ class BinaryHeap:
             return self.a[0]
         return None
 
+class BinaryHeapMin:
+    def __init__(self, size = 1):
+        self.a = Vector(size = size)
+    #return's list of existing leaves indexes
+    def leaves(self, i):
+        res = []
+        if 2 * i + 1 < self.a.len:
+            res.append(2 * i + 1)
+        if 2 * i + 2 < self.a.len:
+            res.append(2 * i + 2)
+        return res
+    #returns index of node's parent
+    def parent(self, i):
+        if i > 0:
+            return (i - 1) // 2
+        return 0
+    def heapify_down(self):
+        leaves = self.leaves(0)
+        curr = 0
+        while any([self.a[leaf] <= self.a[curr] for leaf in leaves]):
+            if len(leaves) == 1:
+                self.a[curr], self.a[leaves[0]] = self.a[leaves[0]], self.a[curr]
+                curr = leaves[0]
+            else:
+                max_leaf = leaves[0] if self.a[leaves[0]] <= self.a[leaves[1]] else leaves[1]
+                self.a[curr], self.a[max_leaf] = self.a[max_leaf], self.a[curr] 
+                curr = max_leaf
+            leaves = self.leaves(curr)
+    def heapify_up(self):
+        curr = self.a.len - 1
+        par = self.parent(curr)
+        while (curr > 0) and (self.a[curr] < self.a[par]):
+            self.a[curr], self.a[par] = self.a[par], self.a[curr]
+            curr = par
+            par = self.parent(curr)
+    def insert(self, val):
+        self.a.put(val)
+        self.heapify_up()
+    def print(self):
+        self.a.pr
+    def extract_min(self):
+        self.a[0], self.a[self.a.len - 1] = self.a[self.a.len - 1], self.a[0]
+        self.a.pop()
+        self.heapify_down()
+    @property
+    def max(self):
+        if self.a.len > 0:
+            return self.a[0]
+        return None
+
 class PriorityQue:
     class Pair:
         def __init__(self, pr, value):
             self.pair = (pr, value)
-        def __gt__(self, op):
-            op1 = self.pair
+        def __gt__(cls, op):
+            op1 = cls.pair
             op2 = op.pair
             if op1[0] > op2[0]:
                 return True
             return False
-        def __ge__(self, op):
-            op1 = self.pair
+        def __ge__(cls, op):
+            op1 = cls.pair
             op2 = op.pair
             if op1[0] >= op2[0]:
                 return True
             return False
+        def __le__(cls, op):
+            return not cls.__gt__(op)
+        def __lt__(cls, op):
+            return not cls.__ge__(op)
+
     def __init__(self):
-        self.que = BinaryHeap()
+        self.que = BinaryHeapMin()
     def insert(self,pr, value):
         self.que.insert(self.Pair(pr, value))
     def peek_max(self):
