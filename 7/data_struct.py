40d0e395a5b44704caf3b578a6f4de3276354a18
y.kareba <y.kareba@g.nsu.ru>
2023-04-16 14:23:50 +0700

    Add functionality do vactor-type and started to write heap

--- 7/data_struct.py
+++ 7/data_struct.py
@@ -64,4 +64,98 @@ class Stack:
             self.stack.rm_head()
             return val
         return None
+
+class Vector:
+    def __init__(self, size = 10):
+        self.__vector = [0] * size
+        self.__head = 0
+        self.__size = size
+        
+    def __alloc(self):
+        tmp = [0] * self.__size * 2
+        for i in range(self.__size):
+            tmp[i] = self.__vector[i] 
+        self.__vector = tmp
+        self.__size *= 2
+    def __shrink(self):
+        tmp = [0] * (self.__size // 2)
+        for i in range(self.__size//4 + 1):
+            tmp[i] = self.__vector[i] 
+        self.__vector = tmp 
+        self.__size = self.__size // 2
+    def put(self, var):
+        if self.__head == self.__size:
+            self.__alloc()
+        self.__vector[self.__head] = var
+        self.__head += 1
+    def pop(self):
+        if self.__head == 0:
+            return None
+        if self.__head <= (self.__size // 4):
+            self.__shrink()
+        self.__head -= 1
+        return(self.__vector[self.__head]) 
+    @property 
+    def pr(self):
+        print(self.__vector)
+    @property
+    def len(self):
+        return self.__head
+    def __getitem__(self, i):
+        return self.__vector[i]
+    def __setitem__(self, i, val):
+        self.__vector[i] = val
+
+
+class BinaryHeap:
+    def __init__(self, size = 0):
+        self.a = Vector(size = size)
+    #return's list of existing leaves indexes
+    def leaves(self, i):
+        res = []
+        if 2 * i + 1 < self.a.len:
+            res.append(2 * i + 1)
+        if 2 * i + 2 < self.a.len:
+            res.append(2 * i + 2)
+        return res
+    #returns index of node's parent
+    def parent(self, i):
+        if i > 0:
+            return (i - 1) // 2
+        return 0
+    def heapify_down(self):
+        leaves = self.leaves(0)
+        curr = 0
+        while any([self.a[leaf] >= self.a[curr] for leaf in leaves]):
+            if len(leaves) == 1:
+                self.a[curr], self.a[leaves[0]] = self.a[leaves[0]], self.a[curr]
+                curr = leaves[0]
+            else:
+                max_leaf = leaves[0] if self.a[leaves[0]] >= self.a[leaves[1]] else leaves[1]
+                self.a[curr], self.a[max_leaf] = self.a[max_leaf], self.a[curr] 
+                curr = max_leaf
+            leaves = self.leaves(curr)
+    def heapify_up(self):
+        curr = self.a.len - 1
+        par = self.parent(curr)
+        while curr > 0 or (self.a[curr] > self.a[par]):
+            self.a[curr], self.a[par] = self.a[par], self.a[curr]
+            curr = par
+            par = self.parent(curr)
+    def insert(self, val):
+        self.a.put(val)
+        self.heapify_up()
+    def print(self):
+        self.a.pr
+    def ch_max(self, val):
+        self.a[0] = val 
+        self.heapify_down()
+
+hp = BinaryHeap(size = 5)
+for i in range(4, 10):
+    hp.insert(i)
+
+hp.print()  
+hp.ch_max(val = 2)
+hp.print()
     
--- 7/station.py
+++ 7/station.py
@@ -1,13 +1,21 @@
 from data_struct import *
-precedence = {None : 3, '!' : 0, '~': 0, '^' : 0, '*' : 1, '/' : 1, '%' : 1, 
+precedence = {'(' : 3, ')': 3, None : 3, '!' : 0, '~': 0, '^' : 0, '*' : 1, '/' : 1, '%' : 1, 
               '+' : 2 , '-' : 2, }
 right_to_left = {'!', '~'}
 stack = Stack()
-inp = input('> ').split(' ')
+inp = input('> ')
+ans = eval(inp)
+inp = inp.split()
 result = ''
 for item in inp:
     if item.isnumeric():
         result += item
+    elif item == '(':
+        stack.push(item)
+    elif item == ')':
+        while stack.seek() != '(':
+            result += stack.pop()
+        stack.pop()
     else:
         n_rtl = (item not in right_to_left) and (precedence[stack.seek()] == precedence[item])
         while ( (precedence[stack.seek()] < precedence[item]) or n_rtl):
